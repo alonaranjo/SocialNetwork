41febac911137d63ed3adab9996f22e286e826f9
alonaranjo <alonso301090@gmail.com>
2023-08-08 18:01:14 -0600

    Some adjustments

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -28,19 +28,10 @@ public class ExceptionMiddleware
             _logger.LogError(ex, ex.Message);
             context.Response.ContentType = CONTENTTYPE;
             context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;           
-            var response = new ApiException(context.Response.StatusCode, ex.Message, GetDetailsByEnvironment(ex.StackTrace?.ToString()));               
+            var response = new ApiException(context.Response.StatusCode, ex.Message, _env.IsDevelopment()? ex.StackTrace?.ToString():"Internal Server Error");               
             var options = new JsonSerializerOptions{ PropertyNamingPolicy = JsonNamingPolicy.CamelCase };   
             var json = JsonSerializer.Serialize(response, options); 
             await context.Response.WriteAsync(json);
         }
-    }
-
-    private string GetDetailsByEnvironment(string details)
-    {
-        if (_env.IsDevelopment())
-        {
-            return details;
-        } 
-        return "Internal Server Error";
     }
 }
